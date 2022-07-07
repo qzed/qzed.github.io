0f2518aa3cd85cf5d6e3c9e6743bda214f3ef495
qzed <qzed@users.noreply.github.com>
2022-07-07 21:05:58 +0000

    deploy: 931d9983d5f2ed59dd03b3087bbc036abaefbae5

--- _next/static/8biQvUfJdTV_n9PvNq08T/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b3d0d47513ed399d.css","static/chunks/pages/index-b3269ba3863d2241.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
