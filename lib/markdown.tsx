6dd27e0f90dbf7a46574326f7c0d8b70c7f7bbf6
Maximilian Luz <luzmaximilian@gmail.com>
2022-08-17 03:25:28 +0200

    Extend markdown support to include latex and code highlighting

--- lib/markdown.tsx
+++ lib/markdown.tsx
@@ -1,11 +1,20 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeStringify from 'rehype-stringify'
 
 import fs from 'fs'
 
 
 export async function markdownToHtml(markdown: string) {
-    const result = await remark().use(html).process(markdown);
+    const result = await unified()
+        .use(remarkParse)
+        .use(remarkRehype, {allowDangerousHtml: true})
+        .use(rehypeHighlight, {plainText: ['txt', 'text']})
+        .use(rehypeStringify, {allowDangerousHtml: true})
+        .process(markdown)
+
     return result.toString();
 }
 
