9081e660287263039acb17a1d8c2033476a751c9
Maximilian Luz <luzmaximilian@gmail.com>
2022-08-18 22:01:20 +0200

    Fix formatting

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,11 @@ import styles from '../styles/footer.module.scss'
 
 
 const Footer = () => {
-    return <footer className={styles.footer}>
-        Copyright © 2022 Maximilian Luz 
+  return (
+    <footer className={styles.footer}>
+      Copyright © 2022 Maximilian Luz 
     </footer>
+  )
 };
 
 export default Footer;
