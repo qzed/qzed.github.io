a34b8e83e88c27a481acf219ff3217760eb310a7
Maximilian Luz <luzmaximilian@gmail.com>
2022-08-26 23:35:11 +0200

    Bundle fonts via fontsource

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
 import '../styles/_globals.scss'
 
+import '@fontsource/playfair-display'
+import '@fontsource/lora'
+import '@fontsource/raleway'
+import '@fontsource/source-code-pro'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
