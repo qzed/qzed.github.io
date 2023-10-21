0ed49fbe91ad023d4803c49012e3f9558895bfc8
Maximilian Luz <luzmaximilian@gmail.com>
2023-10-21 14:38:56 +0200

    Migrate blog index page from pages to app router

---
+++ app/blog/page.tsx
@@ -0,0 +1,77 @@
+import styles from '../../styles/blog-index.module.scss'
+import mdstyles from '../../styles/markdown.module.scss'
+
+import { getAllPosts } from '../../lib/blog'
+import React from 'react'
+import { getMDXComponent } from 'mdx-bundler/client'
+
+import 'katex/dist/katex.css';
+import 'prism-themes/themes/prism-nord.css'
+
+import Link from 'next/link'
+import NavBar from '../../components/NavBar'
+
+import { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Blog | Maximilian Luz',
+  description: 'Blog of Maximilian Luz',
+}
+
+export default async function BlogIndex() {
+  const posts = await getAllPosts()
+  const sorted = posts.sort((a, b) => b.date - a.date)
+
+  return (
+    <main>
+      <NavBar />
+
+      {sorted.map((post) => {
+        const MdxAbstract = getMDXComponent(post.abstract)
+
+        return (
+          <div className={styles.post} key={post.slug}>
+            <div className={styles.column}>
+              <div className={styles.text}>
+                <div>
+                  <div>
+                    <div className={styles.title}>
+                      <h1>
+                        <Link href={`/blog/posts/${post.slug}`}>{post.title}</Link>
+                      </h1>
+                    </div>
+                    <div className={styles.meta}>
+                      <p className={styles.date}>
+                        {
+                          new Date(post.date).toLocaleDateString('en-us', {
+                            month: 'long',
+                            day: 'numeric',
+                            year: 'numeric'
+                          })
+                        }
+                      </p>
+                      <div className={styles.tags}>
+                        {
+                          post.tags?.map((tag) => {
+                            return (<p className={styles.tag} key={tag}>#{tag}</p>)
+                          })
+                        }
+                      </div>
+                    </div>
+                    <div className={mdstyles.markdown}>
+                      <MdxAbstract />
+                    </div>
+                    <div>
+                      <Link href={`/blog/posts/${post.slug}`}>Read more...</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </main>
+  )
+}
