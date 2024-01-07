5dab4ec846e61d06302742f4b70c8628e73de0af
Maximilian Luz <luzmaximilian@gmail.com>
2024-01-07 20:58:02 +0100

    Update copyright year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from '@/styles/footer.module.scss'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Copyright © 2023 Maximilian Luz
+      Copyright © 2023 &ndash; 2024 Maximilian Luz
     </footer>
   )
 }
