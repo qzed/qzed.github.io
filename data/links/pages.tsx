aee4eb998e0bfe00276501882642e90e075eeaa0
Maximilian Luz <luzmaximilian@gmail.com>
2024-02-02 22:39:13 +0100

    Remove dead links

--- data/links/pages.tsx
+++ data/links/pages.tsx
@@ -7,24 +7,9 @@ const pages = [
         icon: icons.Home,
     },
     {
-        title: "About",
-        link: "/about",
-        icon: icons.Person,
-    },
-    {
         title: "Blog",
         link: "/blog",
         icon: icons.EditDocument,
-    },
-    {
-        title: "Publications",
-        link: "/publications",
-        icon: icons.LibraryBooks,
-    },
-    {
-        title: "Projects",
-        link: "/projects",
-        icon: icons.Code,
     },
 ]
 
